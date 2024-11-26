a9803982e7bed4a2028483d55f97bacf3aa263a0
Beakseungmin1 <dhkdwk1456@naver.com>
2024-11-26 21:04:07 +0900

    add : IDamageable, PlayerDamageCalculator.cs, PlayerStat.cs
    플레이어 기본 스크립트 생성

---
+++ Assets/01. Scripts/Player/PlayerDamageCalculator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageCalculator : MonoBehaviour
+{
+    public float TotalDamage;
+    public float basicDamage;
+    public float WeaponDamage;
+
+    public float GetTotalDamage()
+    {
+        TotalDamage = basicDamage + WeaponDamage;
+
+        return TotalDamage;
+
+    }
+
+
+
+}
---
+++ Assets/01. Scripts/Player/PlayerStat.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    private float Damage;
+    private float Health;
+    private float MaxHealth;
+    private float CriticalIncreaseGauge;
+    private float CriticalProbability;
+    private float AttackSpeed;
+    private float NormalMonsterIncreaseDamage;
+    private float BossMonsterIncreaseDamage;
+
+}
