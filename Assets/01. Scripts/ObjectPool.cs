b0d243a1dbec1fa3b1b679b2c81f1996cd2f4850
kkamsogod <9310rlawlgns@gmail.com>
2024-11-27 09:15:40 +0900

    [Update] 오브젝트풀 업데이트

--- Assets/01. Scripts/ObjectPool.cs
+++ Assets/01. Scripts/ObjectPool.cs
@@ -1,18 +1,55 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectPool : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private int initialPoolSize = 10;
+
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    private void Awake()
     {
-        
+        InitializePool();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitializePool()
     {
-        
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject obj = CreateNewObject();
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    private GameObject CreateNewObject()
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.transform.SetParent(this.transform);
+        return obj;
+    }
+
+    public GameObject GetObject()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            return CreateNewObject();
+        }
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
     }
 }
