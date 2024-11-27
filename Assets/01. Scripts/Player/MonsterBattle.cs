bbe2fe0ad8af836741ea262ce147b8293b3db22e
Beakseungmin1 <dhkdwk1456@naver.com>
2024-11-27 17:33:08 +0900

    feat : PlayerBattle, MonsterBattle
    플레이어 공격기능 추가

---
+++ Assets/01. Scripts/Player/MonsterBattle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBattle : MonoBehaviour// IDamageable
+{
+    public float Hp;
+    public float MaxHp = 100;
+
+    public PlayerBattle Player;
+
+    private void Start()
+    {
+        Hp = MaxHp;
+    }
+
+
+    public void TakeDamage(float Damage)
+    {
+
+        if (Hp - Damage > 0)
+        {
+            Hp -= Damage;
+            Debug.Log($"피해를 받음: {Damage}. 현재 HP: {Hp}");
+        }
+        else
+        {
+            Hp -= Damage;
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        // 애니메이션 재생
+        gameObject.SetActive(false);
+    }
+
+    public bool GetActive()
+    {
+        return gameObject.activeInHierarchy;
+    }
+
+
+}
