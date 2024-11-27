f465c45d8ffe68998066823e95498f7ab5e51e6b
leehokyun <urea42@naver.com>
2024-11-27 20:19:41 +0900

    [fix] merge후 지워졌던 grade enum값 재등록

--- Assets/01. Scripts/Util/EnumCollection.cs
+++ Assets/01. Scripts/Util/EnumCollection.cs
@@ -7,3 +7,14 @@ public enum State
     Syncing,    // 동기화 중 (추후 네트워크 추가 시 사용)
     Loading     // 로딩 중
 }
+
+public enum Grade
+{
+    Normal, //일반
+    Uncommon, //희귀
+    Rare, //레어
+    Hero, //영웅
+    Legendary, //전설
+    Mythic, //신화
+    Ultimate //불멸
+}
