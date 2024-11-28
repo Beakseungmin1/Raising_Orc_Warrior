dbd851e815ee0b64c634da9bb43372bf5d685abf
leehokyun <urea42@naver.com>
2024-11-28 14:29:49 +0900

    [feature] HUD UI, StageInfo UI, Character Info UI 디자인 [remove] weaponStatHandler.cs

---
+++ Assets/01. Scripts/Equipment/IEquipment.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEquipment
+{
+    //장착
+    //강화
+    //장착해제
+    //합성
+
+    //장비에서 WeaponDataSO->weapon.cs에서 받아오고-> PlayerDamageCaculator한테 전달.
+    
+    //add, overide, multiple
+}
