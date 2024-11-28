11bb2522c4d820ae223a0b1b236186fd68e332da
Beakseungmin1 <dhkdwk1456@naver.com>
2024-11-28 20:04:00 +0900

    [Add] EnemySO

--- Assets/01. Scripts/Player/EnemyBattle.cs
+++ Assets/01. Scripts/Player/EnemyBattle.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterBattle : MonoBehaviour// IDamageable
+public class EnemyBattle : MonoBehaviour, IDamageable
 {
+    [SerializeField] private EnemySO data; 
+
     public float Hp;
-    public float MaxHp = 100;
+    public float MaxHp;
 
     public PlayerBattle Player;
 
     private void Start()
     {
-        Hp = MaxHp;
+        Hp = data.hp;
+        MaxHp = data.maxHp;
     }
 
 
     public void TakeDamage(float Damage)
     {
+        // 피격 애니메이션 재생 추가예정
 
         if (Hp - Damage > 0)
         {
---
+++ Assets/04. ScriptableObjectAssets/EnemySO.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemy", menuName = "Enemys/Enemy")]
+public class EnemySO : ScriptableObject
+{
+
+    [Header("Enemy information")]
+    public string enemyCode; // 적식별코드
+    public float hp; // 체력
+    public float maxHp; // 최대체력
+
+    [Header("Skill Properties")]
+    public float cooldown; // 쿨다운 시간 (보스가 스킬을 지니고 있을시 사용)
+
+    [Header("Skill Effects")]
+    public GameObject skillEffectPrefab; // 스킬 효과 프리팹
+    public Collider2D effectRange; // 스킬 효과 범위
+    public float damagePercent; // 액티브 스킬: 범위 내 적에게 주는 공격력 비율 (%)
+
+
+}
