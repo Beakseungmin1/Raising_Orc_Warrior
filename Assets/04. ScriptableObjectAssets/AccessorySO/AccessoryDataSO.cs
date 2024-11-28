e0696634f2b36109176394488e36a24fd8352412
kkamsogod <9310rlawlgns@gmail.com>
2024-11-28 20:11:46 +0900

    [Refactor] 웨폰,악세사리 데이터 리팩토링, 베이스 아이템 SO 생성

--- Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
+++ Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
@@ -1,22 +1,13 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Accessory", menuName = "Equipment/New Accessory")]
-public class AccessoryDataSO : ScriptableObject
+public class AccessoryDataSO : BaseItemSO
 {
-    [Header("AccessoryStat")]
-    public float hpAndHpRecoveryIncreaseRate; //체력, 체력 회복량 증가율이다. 보유효과는 장착효과의 30%
-    public float mpAndMpRecoveryIncreaseRate; //보유효과 전체 마나, 마나회복량
-    public float addEXPRate; //보유효과 추가 경험치
+    [Header("Accessory Stats")]
+    public float hpAndHpRecoveryIncreaseRate; // 체력, 체력 회복량 증가율
+    public float mpAndMpRecoveryIncreaseRate; // 마나, 마나 회복량 증가율
+    public float addEXPRate; // 추가 경험치 증가율
 
-    [Header("Info")]
-    public Sprite icon;
-    public Sprite inGameImage;
-    public string accessoryName;
-    public Grade grade;
-    public int upgradeLevel; //현재강화레벨
-    public int rank; //해당 아이템의 등급. 1,2,3,4등급 있음.(1등급이 가장 높음)
-    public int requireCubeForUpgrade; //강화에 필요한 큐브량
-
-    [Header("Stacking")]
-    public int curStackAmount;
+    [Header("Upgrade Info")]
+    public int requireCubeForUpgrade; // 강화에 필요한 큐브 수
 }
