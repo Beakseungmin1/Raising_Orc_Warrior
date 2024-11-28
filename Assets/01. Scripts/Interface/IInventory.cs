22aea80e76d60a5815a30c3f941c61c6d512b706
kkamsogod <9310rlawlgns@gmail.com>
2024-11-28 20:11:09 +0900

    [Feat] 인벤토리 추가

---
+++ Assets/01. Scripts/Interface/IInventory.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+public interface IInventory<T> where T : BaseItemSO
+{
+    void AddItem(T item); // 아이템 추가
+    void RemoveItem(T item); // 아이템 삭제
+    T GetItem(string itemName); // 이름으로 아이템 검색
+    List<T> GetAllItems(); // 모든 아이템 가져오기
+    int GetItemCount(); // 현재 아이템 개수
+}
---
+++ Assets/01. Scripts/Inventory/WeaponInventory.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+public class WeaponInventory : IInventory<WeaponDataSO>
+{
+    private List<WeaponDataSO> weapons = new List<WeaponDataSO>();
+
+    public void AddItem(WeaponDataSO item)
+    {
+        weapons.Add(item);
+    }
+
+    public void RemoveItem(WeaponDataSO item)
+    {
+        weapons.Remove(item);
+    }
+
+    public WeaponDataSO GetItem(string itemName)
+    {
+        return weapons.Find(weapon => weapon.itemName == itemName);
+    }
+
+    public List<WeaponDataSO> GetAllItems()
+    {
+        return new List<WeaponDataSO>(weapons);
+    }
+
+    public int GetItemCount()
+    {
+        return weapons.Count;
+    }
+}
