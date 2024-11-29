08f7bd8c58d03ca96307375eaef8b480df771425
leehokyun <urea42@naver.com>
2024-11-29 14:50:36 +0900

    [feature] currency매니저
    재화별 추가재거 메서드 구현

--- Assets/01. Scripts/Manager/CurrencyManager.cs
+++ Assets/01. Scripts/Manager/CurrencyManager.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class CurrencyManager : Singleton<CurrencyManager>
 {
+    public int curGold = 0; //능력치 강화용
+    public int curEmerald = 0; //스킬, 동료 강화용
+    public int curCube = 0; //장비 강화용
+    public int curDiamond = 0; //뽑기용
 
+    public void AddGold(int addAmount)
+    {
+        curGold += addAmount;
+    }
+
+    public void SubtractGold(int subtractAmount)
+    {
+        curGold -= subtractAmount;
+    }
+
+    public void AddEmerald(int addAmount)
+    {
+        curEmerald += addAmount;
+    }
+
+    public void SubtractEmerald(int subtractAmount)
+    {
+        curEmerald -= subtractAmount;
+    }
+
+    public void AddCube(int addAmount)
+    {
+        curCube += addAmount;
+    }
+
+    public void SubtractCube(int subtractAmount)
+    {
+        curCube -= subtractAmount;
+    }
+
+    public void AddDiamond(int addAmount)
+    {
+        curDiamond += addAmount;
+    }
+
+    public void SubtractDiamond(int subtractAmount)
+    {
+        curDiamond -= subtractAmount;
+    }
 }
