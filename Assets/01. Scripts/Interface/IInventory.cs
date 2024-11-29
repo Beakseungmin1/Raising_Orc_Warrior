890410abd75d2043730b4e87277fdd8ebe07d556
kkamsogod <9310rlawlgns@gmail.com>
2024-11-29 19:55:32 +0900

    [Refactor] 인벤토리 리펙토링

--- Assets/01. Scripts/Interface/IInventory.cs
+++ Assets/01. Scripts/Interface/IInventory.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-public interface IInventory<T> where T : BaseItemSO
+public interface IInventory<T> where T : BaseItemDataSO
 {
     void AddItem(T item); // 아이템 추가
     void RemoveItem(T item); // 아이템 삭제
