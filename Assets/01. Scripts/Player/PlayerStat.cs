d587f9271377b08217ebc1404b66f6f91e154d2a
Beakseungmin1 <dhkdwk1456@naver.com>
2024-11-29 20:07:53 +0900

    [Feat] PlayerStat.cs
    플레이어 스탯 스크립트 수치 변경 메서드 추가

--- Assets/01. Scripts/Player/PlayerStat.cs
+++ Assets/01. Scripts/Player/PlayerStat.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class PlayerStat : MonoBehaviour
 {
-    private float Damage;
-    private float Health;
-    private float MaxHealth;
-    private float Mana;
-    private float MaxMan;
-    private float Level;
-    private float Exp;
-    private float CriticalIncreaseDamage;
-    private float CriticalProbability;
-    private float AttackSpeed;
-    private float NormalMonsterIncreaseDamage;
-    private float BossMonsterIncreaseDamage;
+    private float level;
+    private float exp;
+    private float attackPower;
+    private float health;
+    private float maxHealth;
+    private float healthRegeneration;
+    private float criticalProbability;
+    private float criticalIncreaseDamage;
+    private float mana;
+    private float maxMana;
+    private float manaRegeneration;
+    private float Avoid;
+    private float extraGoldGainRate;
+    private float extraExpRate;
+    private float attackSpeed;
+    private float normalMonsterIncreaseDamage;
+    private float bossMonsterIncreaseDamage;
 
+    public void EquipWeaponValue(Weapon weapon)
+    {
+        float equipWeaponPower = attackPower * (weapon.atkIncreaseRate / 100);
+        attackPower += equipWeaponPower;
 
+    }
 
+    public void unEquipWeaponValue(Weapon weapon)
+    {
+        float equipWeaponPower = attackPower * (weapon.atkIncreaseRate / 100);
+        attackPower -= equipWeaponPower;
 
+    }
 
+    public void HoldIncreaseWeaponValue(Weapon weapon)
+    {
+        float holdWeaponPower = attackPower * (weapon.atkIncreaseRate / 30 / 100);
+        attackPower += holdWeaponPower;
+        
+        float holdIncreaseCriticalPower = criticalIncreaseDamage * (weapon.criticalDamageBonus / 100);
+        criticalIncreaseDamage += holdIncreaseCriticalPower;
 
-
-
-
+        float holdIncreaseGoldGain = extraGoldGainRate * (weapon.increaseGoldGainRate / 100);
+        extraGoldGainRate += holdIncreaseGoldGain;
+    }
 
 }
