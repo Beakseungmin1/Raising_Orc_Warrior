909b29351bfa1d22356a3c5837db61abfccbf1a3
kkamsogod <9310rlawlgns@gmail.com>
2024-11-29 19:54:47 +0900

    [Refactor] 액세사리,베이스, 스킬데이타 SO 리팩토링

--- Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
+++ Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Accessory", menuName = "Equipment/New Accessory")]
-public class AccessoryDataSO : BaseItemSO
+public class AccessoryDataSO : BaseItemDataSO
 {
-    [Header("Accessory Stats")]
-    public float hpAndHpRecoveryIncreaseRate; // 체력, 체력 회복량 증가율
-    public float mpAndMpRecoveryIncreaseRate; // 마나, 마나 회복량 증가율
-    public float addEXPRate; // 추가 경험치 증가율
+    [Header("Equip Effects")] // 착용 효과
+    public float equipHpAndHpRecoveryIncreaseRate; // 체력 및 체력 회복량 증가율 (장착 효과)
+
+    [Header("Ownership Effects")] // 보유 효과
+    public float passiveMpAndMpRecoveryIncreaseRate; // 마나 및 마나 회복량 증가율 (보유 효과)
+    public float passiveAddEXPRate; // 추가 경험치 증가율 (보유 효과)
+
+    [Header("General Info")]
+    public int rank;
 
     [Header("Upgrade Info")]
     public int requireCubeForUpgrade; // 강화에 필요한 큐브 수
