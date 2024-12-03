ea3b9108a4dfdd1c2f8c43a08a3b688211054278
leehokyun <urea42@naver.com>
2024-12-03 12:44:33 +0900

    [feature] chapterSO, stageSO 추가

---
+++ Assets/04. ScriptableObjectAssets/ChapterSO/ChapterSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultChapterSO", menuName = "ChapterSO", order = 1)]
+public class ChapterSO : ScriptableObject
+{
+    public int chapterNum;
+    public StageSO[] stageSOs;
+    public AudioClip chapterMusic;
+
+    [Header("BackGround")]
+    public Sprite bgSprite; //¹è°æ
+    public string chapterName;
+}
