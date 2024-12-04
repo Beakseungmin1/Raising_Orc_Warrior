270ba33ae8a8e769b61807989af590c9a49c8eb2
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-04 20:13:44 +0900

    [add] PlayerUpgeadeStat.cs
    플레이어 업그레이드 스탯 추가

---
+++ Assets/01. Scripts/Player/PlayerUpgradeStat.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUpgradeStat : MonoBehaviour
+{
+    private int attack;
+    private int health;
+    private int healthRegeneration;
+    private int criticalIncreaseDamage;
+    private int criticalProbability;
+    private int bluecriticalIncreaseDamage;
+    private int bluecriticalProbability;
+}
