3d82f221c7f41b1edb2ce235271ff324947b50e5
leehokyun <urea42@naver.com>
2024-12-05 20:07:51 +0900

    [feature] summon클래스 작업 초안, SummonGridCellUI디자인

---
+++ Assets/SummonGridCellUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonGridCellUI : UIBase
+{
+    //weaponSO¸¦ ¹Þ¾Æ¿È
+    //¶Ç´Â skillSO¸¦ ¹Þ¾Æ¿È
+    //¶Ç´Â accSO¸¦ ¹Þ¾Æ¿È
+
+    
+}
