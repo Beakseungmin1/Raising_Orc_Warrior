1327cbb732a80eb0947a8aea3bbbeb5791d81c37
kkamsogod <9310rlawlgns@gmail.com>
2024-12-05 20:17:58 +0900

    [Chore] 베이스아이템, 스킬데이타 SO 수정

--- Assets/04. ScriptableObjectAssets/BaseItemDataSO.cs
+++ Assets/04. ScriptableObjectAssets/BaseItemDataSO.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public abstract class BaseItemDataSO : ScriptableObject
 {
-    [Header("Base Info")]
+    [Header("Basic Information")]
     public string itemName; // 아이템 이름
     public Sprite icon; // 아이템 아이콘
     public Sprite inGameImage; // 인게임 이미지
     public Grade grade; // 등급
+    public Color gradeColor; // 등급별 색상
+
+    [Header("Upgrade Information")]
+    public Sprite currencyIcon; // 강화에 필요한 재화 아이콘
+    public int requiredCurrencyForUpgrade; // 강화에 필요한 재화 수량
+    public int currentLevel; // 현재 레벨
 }
--- Assets/04. ScriptableObjectAssets/SkillSO/SkillDataSO.cs
+++ Assets/04. ScriptableObjectAssets/SkillSO/SkillDataSO.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 public class SkillDataSO : BaseItemDataSO
 {
     [Header("Skill Details")]
-    public string description; // 스킬 데미지 설명
-    public string description2; // 스킬 설명
+    public string description; // 스킬 설명
+    public string effectDescription; // 스킬 효과 설명    
     public SkillType skillType; // 스킬 타입 (액티브, 버프, 패시브)
     public EffectType effectType; // 스킬 이펙트 타입
 
     [Header("Activation Condition")]
     public ActivationCondition activationCondition; // 발동 조건 (쿨다운, 공격 횟수)
-
-    [Header("Skill Properties")]
     public float cooldown; // 쿨다운 시간
     public int requiredHits; // 필요한 공격 횟수
-    public float periodicInterval; // 주기적 발동 시간 (패시브 스킬)
-    public int manaCost; // 마나 소모량
+    public float periodicInterval;
 
-    [Header("Skill Effects")]
-    public GameObject effectPrefab; // 이펙트 프리팹
-    public float effectRange; // 효과 범위
+    [Header("Skill Properties")]
     public float damagePercent; // 공격력 비율
+    public int manaCost; // 마나 소모량
+    public float effectRange; // 효과 범위
     public float buffDuration; // 버프 지속 시간
     public float attackIncreasePercent; // 공격력 증가 비율
 
     [Header("Upgrade Info")]
-    public int requireEmelardForUpgrade; // 업그레이드에 필요한 에메랄드 수
-    public int requireSkillCardsForUpgrade; // 업그레이드에 필요한 스킬 카드 수
+    public int requireSkillCardsForUpgrade; // 업그레이드에 필요한 카드수
+    public int maxLevel; // 최대 레벨    
+
+    [Header("Visual and Effects")]
+    public GameObject effectPrefab; // 이펙트 프리팹
 }
