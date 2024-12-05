3f1eb4966c1586cc1e23181b88b5003b7d08262c
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-05 20:14:40 +0900

    [feat] 레벨 ui 업데이트 기능 추가

--- Assets/01. Scripts/UI/PlayerLevelInfoUI.cs
+++ Assets/01. Scripts/UI/PlayerLevelInfoUI.cs
@@ -1,15 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerLevelInfoUI : MonoBehaviour
+public class PlayerLevelInfoUI : Singleton<PlayerLevelInfoUI>
 {
-    public Text UILevel;
-    public GameObject ExpBar;
-    public Text UIExppercent;
-    public Text UIcurExp;
-    public Text UIneedExp;
+    private PlayerStat stat;
+
+    [SerializeField] private TextMeshProUGUI UILevelTxt;
+    [SerializeField] private Slider ExpBar;
+    [SerializeField] private TextMeshProUGUI UIExppercentTxt;
+    [SerializeField] private TextMeshProUGUI UICurExpTxt;
+    [SerializeField] private TextMeshProUGUI UINeedExpTxt;
+
+    private void Awake()
+    {
+        stat = PlayerobjManager.Instance.Player.stat;
+    }
+
+
+    public void UpdateLevelUI()
+    {
+        if (stat != null)
+        {
+            UILevelTxt.text = stat.GetLevel().ToString();
+
+            float currentExp = stat.GetExp();
+            float needExp = stat.GetNeedExp();
+
+            //현재 경험치가 총경험치의 몇% 인지 계산
+            float percentage = needExp > 0 ? (currentExp / needExp) * 100 : 0;
+
+            //경험치 표시가 100% 을 넘어가지 못하게함
+            float ExppercentTxt = percentage >= 100 ? 100 : percentage;
+
+            ExpBar.value = percentage / 100;
+
+            UIExppercentTxt.text = ExppercentTxt.ToString("F2");
+
+            UICurExpTxt.text = currentExp.ToString();
+            UINeedExpTxt.text = needExp.ToString();
+        }
+    }
+
+
 
 
 
