356bed878d2cb6648e8f9bcda39d4141f8cdbcc7
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-05 15:55:13 +0900

    [Add] 레벨업ui 관리 스크립트 추가

---
+++ Assets/01. Scripts/UI/PlayerLevelInfoUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLevelInfoUI : MonoBehaviour
+{
+    public Text UILevel;
+    public GameObject ExpBar;
+    public Text UIExppercent;
+    public Text UIcurExp;
+    public Text UIneedExp;
+
+
+
+
+}
