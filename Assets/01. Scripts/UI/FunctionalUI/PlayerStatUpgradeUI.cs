5a2958a00d8ba79b6cc4326a153374bea9663491
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-09 20:20:31 +0900

    [feat] 업그레이드 버튼에 ui 업데이트 기능 추가

--- Assets/01. Scripts/UI/FunctionalUI/PlayerStatUpgradeUI.cs
+++ Assets/01. Scripts/UI/FunctionalUI/PlayerStatUpgradeUI.cs
@@ -12,34 +12,51 @@ public class PlayerStatUpgradeUI : MonoBehaviour
     public UpgradeTabUI blueCriticalDamageUi;
     public UpgradeTabUI blueCriticalProbabilityUi;
 
-
+    private void Start()
+    {
+        //юс╫ц
+        attackUi.UpdateAttackStatUI();
+        healthUi.UpdateHealthStatUI();
+        healthRegenUi.UpdateHealthRegenerationUI();
+        criticalDamageUi.UpdateCriticalIncreaseDamageUI();
+        criticalProbabilityUi.UpdateCriticalProbabilityUI();
+        blueCriticalDamageUi.UpdateblueCriticalIncreaseDamageStatUI();
+        blueCriticalProbabilityUi.UpdateblueCriticalProbabilityStatUI();
+    }
     public void OnAttackLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.AttackLevelUp();
+        attackUi.UpdateAttackStatUI();
     }
     public void OnHealthLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.HealthLevelUp();
+        healthUi.UpdateHealthStatUI();
     }
     public void OnHealthRegenLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.HealthRegenerationLevelUp();
+        healthRegenUi.UpdateHealthRegenerationUI();
     }
     public void OnCriticalDamageLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.CriticalIncreaseDamageLevelUp();
+        criticalDamageUi.UpdateCriticalIncreaseDamageUI();
     }
     public void OnCriticalProbabilityLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.CriticalProbabilityLevelUp();
+        criticalProbabilityUi.UpdateCriticalProbabilityUI();
     }
     public void OnBlueCriticalDamageLevelUpButton()
     {
         PlayerobjManager.Instance.Player.stat.BlueCriticalIncreaseDamageLevelUp();
+        blueCriticalDamageUi.UpdateblueCriticalIncreaseDamageStatUI();
     }
     public void OnBlueCriticalProbabilityLevelUpButto()
     {
         PlayerobjManager.Instance.Player.stat.BlueCriticalProbabilityLevelUp();
+        blueCriticalProbabilityUi.UpdateblueCriticalProbabilityStatUI();
     }
 
 
