23575ea9140e5bf9d753c776e5f8a3955cbd81fd
kkamsogod <9310rlawlgns@gmail.com>
2024-12-09 20:32:02 +0900

    [Refactor] 인터페이스 IEnhanceable 리펙토링

--- Assets/01. Scripts/Interface/IEnhanceable.cs
+++ Assets/01. Scripts/Interface/IEnhanceable.cs
@@ -1,5 +1,8 @@
 public interface IEnhanceable
 {
+    BaseItemDataSO BaseData { get; }
+    int EnhancementLevel { get; }
+    int StackCount { get; }
     bool CanEnhance(); // 강화 가능 여부 확인
     bool Enhance(); // 강화 실행
 }
