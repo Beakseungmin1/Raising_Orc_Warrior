2b5a2b60db23f68622e32ace78660e9925b2e7c7
leehokyun <urea42@naver.com>
2024-12-09 22:04:29 +0900

    [refactor] summonSlot 제너릭 받는 형태로 리팩토링
    제너릭 메서드로 변경하여
    weaponDataSO, AccessaryDataSO, SkillDataSO 모든 타입을 받을 수 있도록 구현.

--- Assets/01. Scripts/UI/SummonSlot.cs
+++ Assets/01. Scripts/UI/SummonSlot.cs
@@ -6,7 +6,7 @@ using TMPro;
 
 public class SummonSlot : UIBase
 {
-    public WeaponDataSO weaponDataSO;
+    public BaseItemDataSO baseItemDataSO;
 
     public TextMeshProUGUI gradeTxt;
     public TextMeshProUGUI rankTxt;
@@ -21,23 +21,65 @@ public class SummonSlot : UIBase
         rankLabel.enabled = false;
     }
 
-    public void SetSlot(WeaponDataSO so)
+    public void SetSlot<T>(T so) where T : BaseItemDataSO
     {
-        Debug.Log("SetSLot");
-        weaponDataSO = so;
+        baseItemDataSO = so;
+
+        if (so is WeaponDataSO weaponDataSO)
+        {
+            SetWeaponSlot(weaponDataSO);
+        }
+        else if (so is AccessoryDataSO accessoryDataSO)
+        {
+            SetAccessorySlot(accessoryDataSO);
+        }
+        else if (so is SkillDataSO skillDataSO)
+        {
+            SetSkillSlot(skillDataSO);
+        }
+        else
+        {
+            Debug.LogWarning("Unsupported data type for SetSlot.");
+        }
+    }
+
+    private void SetWeaponSlot(WeaponDataSO weaponDataSO)
+    {
         gradeTxt.text = weaponDataSO.grade.ToString();
         rankTxt.text = weaponDataSO.rank.ToString();
         image.sprite = weaponDataSO.icon;
+        EnableUI();
+    }
+
+    private void SetAccessorySlot(AccessoryDataSO accessoryDataSO)
+    {
+        gradeTxt.text = accessoryDataSO.grade.ToString();
+        rankTxt.text = accessoryDataSO.rank.ToString();
+        image.sprite = accessoryDataSO.icon;
+        EnableUI();
+    }
+
+    private void SetSkillSlot(SkillDataSO skillDataSO)
+    {
+        gradeTxt.text = skillDataSO.grade.ToString();
+        rankTxt.text = ""; // ½ºÅ³¿¡ ·©Å©°¡ ¾øÀ¸¸é ºóÄ­ Ã³¸®
+        image.sprite = skillDataSO.icon;
+        EnableUI();
+    }
+
+    private void EnableUI()
+    {
         image.enabled = true;
         gradeTxt.enabled = true;
         rankTxt.enabled = true;
         rankLabel.enabled = true;
     }
 
+
     public void ClearSlot()
     {
         Debug.Log("ClearSLot");
-        weaponDataSO = null;
+        baseItemDataSO = null;
         gradeTxt.text = null;
         rankTxt.text = null;
         image.sprite = null;
