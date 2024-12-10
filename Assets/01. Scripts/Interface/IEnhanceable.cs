456bcd11e5cc600fa3236f57a8b932d57697c866
kkamsogod <9310rlawlgns@gmail.com>
2024-12-10 20:13:56 +0900

    [Refactor] 인터페이스 리펙토링

--- Assets/01. Scripts/Interface/IEnhanceable.cs
+++ Assets/01. Scripts/Interface/IEnhanceable.cs
@@ -1,8 +1,8 @@
-public interface IEnhanceable
+public interface IEnhanceable : IStackable
 {
     BaseItemDataSO BaseData { get; }
-    int EnhancementLevel { get; }
-    int StackCount { get; }
+    int EnhancementLevel { get; } // 현재 강화 레벨
+    int RequiredCurrencyForUpgrade { get; }
     bool CanEnhance(); // 강화 가능 여부 확인
     bool Enhance(); // 강화 실행
 }
--- Assets/01. Scripts/Interface/IFusable.cs
+++ Assets/01. Scripts/Interface/IFusable.cs
@@ -1,5 +1,4 @@
 public interface IFusable
 {
-    bool CanFuse(); // 합성 가능 여부 확인
-    bool Fuse(); // 합성 실행
+    bool Fuse(int materialCount);
 }
