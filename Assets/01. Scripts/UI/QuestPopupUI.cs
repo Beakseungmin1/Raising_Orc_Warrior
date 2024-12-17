9aa10cab15e279bf4ece8831e85039d71e80d0ad
leehokyun <urea42@naver.com>
2024-12-17 14:09:21 +0900

    [feature] 퀘스트 UI들 연결

--- Assets/01. Scripts/UI/QuestPopupUI.cs
+++ Assets/01. Scripts/UI/QuestPopupUI.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class QuestPopupUI : UIBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ExitBtn()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        UIManager.Instance.Hide<DimmedUI>();
+        Hide();
     }
 }
