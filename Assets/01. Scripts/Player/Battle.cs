140c5df6cc4789002682aa48c7885080bb4b4a76
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-17 20:38:38 +0900

    [feat] 애니메이션에 넣을 공격 스크립트 추가

---
+++ Assets/01. Scripts/Player/Battle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+
+public class Battle : MonoBehaviour
+{
+    public PlayerBattle player;
+    public EnemyBoss enemy;
+
+
+    public void TriggerAttack()
+    {
+        if (player != null)
+        {
+            player.OnPlayerAttack.Invoke();
+        }
+        else if (enemy != null)
+        {
+            enemy.OnEnemyAttack.Invoke();
+        }
+        else
+        {
+            return;
+        }
+    }
+
+
+
+}
