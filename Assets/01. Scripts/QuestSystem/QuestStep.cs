f5040c4769b2901111b8f74aa4ac5e4ebd512f80
leehokyun <urea42@naver.com>
2024-12-20 19:37:58 +0900

    [feature] QuestStep 초기화코드, 퀘스트스텝 생성시 초기화.

--- Assets/01. Scripts/QuestSystem/QuestStep.cs
+++ Assets/01. Scripts/QuestSystem/QuestStep.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public abstract class QuestStep : MonoBehaviour
 {
     private bool isFinished = false;
+    private string questId;
+
+    public void InitializeQuestStep(string questId)
+    {
+        this.questId = questId;
+    }
 
     protected void FinishQuestStep()
     {
         if (!isFinished)
         {
             isFinished = true; 
+            GameEventsManager.Instance.questEvents.AdvanceQuest(questId);
             Destroy(this.gameObject);
         }
     }
