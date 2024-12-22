f876d81d17a0247ff74d344fea938f5fd780966a
Beakseungmin1 <dhkdwk1456@naver.com>
2024-12-22 23:34:08 +0900

    [feat] 보스 애니메이션 추가

--- Assets/01. Scripts/Enemy/EnemyDungeonBoss.cs
+++ Assets/01. Scripts/Enemy/EnemyDungeonBoss.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class EnemyDungeonBoss : MonoBehaviour, IEnemy
 {
+    private enum Pattern
+    {
+        Pattern1,
+        Pattern2,
+        Pattern3
+    }
+
     public EnemySO enemySO;
 
     [Header("Enemy information")]
@@ -23,18 +30,21 @@ public class EnemyDungeonBoss : MonoBehaviour, IEnemy
     [SerializeField] private Collider2D effectRange; // 스킬 효과 범위
     [SerializeField] private float damagePercent; // 액티브 스킬: 범위 내 적에게 주는 공격력 비율 (%)
 
+    private Pattern enemyPattern;
+
     public Action OnEnemyDeath;
 
     public Action OnEnemyAttack;
 
     private PlayerBattle player;
 
-    private int Hitcounter = 0;
+    float patternTime = 10f; // 패턴 유지시간
+    float toTime; // 패턴 시간 계산용 변수
 
-    private void Awake()
-    {
-        OnEnemyDeath += RegenManager.Instance.EnemyKilled;
-    }
+    //private void Awake()
+    //{
+    //    OnEnemyDeath += RegenManager.Instance.EnemyKilled;
+    //}
 
     private void Start()
     {
@@ -42,17 +52,40 @@ public class EnemyDungeonBoss : MonoBehaviour, IEnemy
         OnEnemyAttack = GiveDamageToPlayer;
     }
 
-    public void TakeDamage(BigInteger Damage)
+    private void Update()
     {
-        // 기본 레이어
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        toTime += Time.deltaTime;
 
-        // IDLE 애니메이션이 재생 중 일때의 로직
-        if (stateInfo.IsName("IDLE"))
+        switch (enemyPattern)
         {
-            animator.SetTrigger("3_Damaged");
+            case Pattern.Pattern1:
+                SetfalseAnimation();
+                animator.SetBool("Pattern1", true);
+                break;
+            case Pattern.Pattern2:
+                SetfalseAnimation();
+                animator.SetBool("Pattern2", true);
+                break;
+            case Pattern.Pattern3:
+                SetfalseAnimation();
+                animator.SetBool("Pattern3", true);
+                break;
         }
 
+        SwitchPattern();
+    }
+
+    public void SetfalseAnimation()
+    {
+        animator.SetBool("Pattern1", false);
+        animator.SetBool("Pattern2", false);
+        animator.SetBool("Pattern3", false);
+    }
+
+
+
+    public void TakeDamage(BigInteger Damage)
+    {
         if (hp - Damage > 0)
         {
             hp -= Damage;
@@ -74,18 +107,12 @@ public class EnemyDungeonBoss : MonoBehaviour, IEnemy
 
     public void GiveDamageToPlayer()
     {
-        if (Hitcounter >= 2)
-        {
-            player.TakeKnockbackDamage(10, 0.5f); //안에 넣은 값은 임시값 이후 (몬스터고유데미지, 몬스터고유넉백시간) 으로 조정예정
-            Hitcounter = 0;
-            //Debug.Log("강력한 공격발동 현재 히트 : " + Hitcounter);
-        }
-        else
-        {
-            player.TakeDamage(10); // 안에 넣은 값은 임시값
-            Hitcounter++;
-            //Debug.Log("일반공격 현재 히트 : " + Hitcounter);
-        }
+        player.TakeDamage(10); // 안에 넣은 값은 임시값
+    }
+
+    public void GiveKnockDamageToPlayer()
+    {
+        player.TakeKnockbackDamage(10, 0.5f); //안에 넣은 값은 임시값 이후 (몬스터고유데미지, 몬스터고유넉백시간) 으로 조정예정
     }
 
     public BigInteger GiveExp()
@@ -95,7 +122,6 @@ public class EnemyDungeonBoss : MonoBehaviour, IEnemy
 
     public void Die()
     {
-        animator.SetTrigger("4_Death");
         ObjectPool.Instance.ReturnObject(gameObject);
         GameEventsManager.Instance.enemyEvents.EnemyKilled();
     }
@@ -124,6 +150,24 @@ public class EnemyDungeonBoss : MonoBehaviour, IEnemy
         effectRange = enemySO.effectRange;
         damagePercent = enemySO.damagePercent;
     }
+
+    void SwitchPattern()
+    {
+        if (toTime > patternTime)
+        {
+            Pattern newPattern;
+            do
+            {
+                int ran = UnityEngine.Random.Range(0, Enum.GetValues(typeof(Pattern)).Length);
+                newPattern = (Pattern)ran;
+
+            } while (newPattern == enemyPattern);
+
+            enemyPattern = newPattern;
+            toTime = 0;
+        }
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
