9520c0bdce86974c8a3096d329f649c3d003a24d
leehokyun <urea42@naver.com>
2024-12-23 11:20:13 +0900

    [fix] 소환시 diamond 차감하는 로직 구현

--- Assets/01. Scripts/UI/Main_ShopUI.cs
+++ Assets/01. Scripts/UI/Main_ShopUI.cs
@@ -62,52 +62,112 @@ public class Main_ShopUI : UIBase
 
     public void OnWeaponSummon(int summonCount)
     {
-        List<WeaponDataSO> weaponDataSOs = new List<WeaponDataSO>();
-        weaponDataSOs = summon.SummonWeaponDataSOList(summonCount); //웨폰데이터 리스트가 세팅된다. //OK
+        float price = 0;
 
-        SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
-        summonPopupUI.curSummoningItemType = ItemType.Weapon;
-        summonPopupUI.SetSlotAsCount(summonCount);
-        summonPopupUI.ClearSlotData();
-        summonPopupUI.StartSetDataSOs(weaponDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
-
-        if (SummonDataManager.Instance.GetLevel(ItemType.Weapon) < 50)
+        switch (summonCount)
         {
-            SummonDataManager.Instance.AddExperience(ItemType.Weapon, summonCount);
+            case 1:
+                price = 50f;
+                break;
+            case 11:
+                price = 500f;
+                break;
+            default:
+                price = 1500f;
+                break;
+        }
+
+        if (CurrencyManager.Instance.GetCurrency(CurrencyType.Diamond) >= price)
+        {
+            CurrencyManager.Instance.SubtractCurrency(CurrencyType.Diamond, price);
+
+            List<WeaponDataSO> weaponDataSOs = new List<WeaponDataSO>();
+            weaponDataSOs = summon.SummonWeaponDataSOList(summonCount); //웨폰데이터 리스트가 세팅된다. //OK
+
+            SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
+            summonPopupUI.curSummoningItemType = ItemType.Weapon;
+            summonPopupUI.SetSlotAsCount(summonCount);
+            summonPopupUI.ClearSlotData();
+            summonPopupUI.StartSetDataSOs(weaponDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
+
+            if (SummonDataManager.Instance.GetLevel(ItemType.Weapon) < 50)
+            {
+                SummonDataManager.Instance.AddExperience(ItemType.Weapon, summonCount);
+            }
         }
     }
 
     public void OnAccSummon(int summonCount)
     {
-        List<AccessoryDataSO> accessoryDataSOs = new List<AccessoryDataSO>();
-        accessoryDataSOs = summon.SummonAccessoryDataSOList(summonCount);
+        float price = 0;
 
-        SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
-        summonPopupUI.curSummoningItemType = ItemType.Accessory;
-        summonPopupUI.SetSlotAsCount(summonCount);
-        summonPopupUI.ClearSlotData();
-        summonPopupUI.StartSetDataSOs(accessoryDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
-
-        if (SummonDataManager.Instance.GetLevel(ItemType.Accessory) < 50)
+        switch (summonCount)
         {
-            SummonDataManager.Instance.AddExperience(ItemType.Accessory, summonCount);
+            case 1:
+                price = 50f;
+                break;
+            case 11:
+                price = 500f;
+                break;
+            default:
+                price = 1500f;
+                break;
+        }
+
+        if (CurrencyManager.Instance.GetCurrency(CurrencyType.Diamond) >= price)
+        {
+            CurrencyManager.Instance.SubtractCurrency(CurrencyType.Diamond, price);
+
+            List<AccessoryDataSO> accessoryDataSOs = new List<AccessoryDataSO>();
+            accessoryDataSOs = summon.SummonAccessoryDataSOList(summonCount);
+
+            SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
+            summonPopupUI.curSummoningItemType = ItemType.Accessory;
+            summonPopupUI.SetSlotAsCount(summonCount);
+            summonPopupUI.ClearSlotData();
+            summonPopupUI.StartSetDataSOs(accessoryDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
+
+            if (SummonDataManager.Instance.GetLevel(ItemType.Accessory) < 50)
+            {
+                SummonDataManager.Instance.AddExperience(ItemType.Accessory, summonCount);
+            }
         }
     }
 
     public void OnSkillCardSummon(int summonCount)
     {
-        List<SkillDataSO> skillDataSOs = new List<SkillDataSO>();
-        skillDataSOs = summon.SummonSkillDataSOList(summonCount);
+        float price = 0;
 
-        SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
-        summonPopupUI.curSummoningItemType = ItemType.Skill;
-        summonPopupUI.SetSlotAsCount(summonCount);
-        summonPopupUI.ClearSlotData();
-        summonPopupUI.StartSetDataSOs(skillDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
-
-        if (SummonDataManager.Instance.GetLevel(ItemType.Skill) < 50)
+        switch (summonCount)
         {
-            SummonDataManager.Instance.AddExperience(ItemType.Skill, summonCount);
+            case 1:
+                price = 300f;
+                break;
+            case 11:
+                price = 3000f;
+                break;
+            default:
+                price = 9000f;
+                break;
+        }
+
+        if (CurrencyManager.Instance.GetCurrency(CurrencyType.Diamond) >= price)
+        {
+            CurrencyManager.Instance.SubtractCurrency(CurrencyType.Diamond, price);
+
+            List<SkillDataSO> skillDataSOs = new List<SkillDataSO>();
+            skillDataSOs = summon.SummonSkillDataSOList(summonCount);
+
+            SummonPopupUI summonPopupUI = UIManager.Instance.Show<SummonPopupUI>();
+            summonPopupUI.curSummoningItemType = ItemType.Skill;
+            summonPopupUI.SetSlotAsCount(summonCount);
+            summonPopupUI.ClearSlotData();
+            summonPopupUI.StartSetDataSOs(skillDataSOs); //그 생성된 웨폰데이터를 바탕으로 웨폰데이터를 세팅해준다.
+
+            if (SummonDataManager.Instance.GetLevel(ItemType.Skill) < 50)
+            {
+                SummonDataManager.Instance.AddExperience(ItemType.Skill, summonCount);
+            }
         }
     }
 
