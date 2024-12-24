a310c7e5fff5af7e410261152c5db0386bf09fac
kkamsogod <9310rlawlgns@gmail.com>
2024-12-24 19:39:19 +0900

    [Chore] 스킬, 장비, 에너미 리팩토링

--- Assets/01. Scripts/Manager/RegenManager.cs
+++ Assets/01. Scripts/Manager/RegenManager.cs
@@ -7,55 +7,56 @@ public class RegenManager : Singleton<RegenManager>
 {
     private ChapterSO curChapterSO;
     [SerializeField] private EnemySO[] enemySOs;
-    [SerializeField] private Transform[] enemyRegenPoss;
 
-    [SerializeField] private Transform field;
+    public int totalEnemies = 0; // 해당 스테이지 적 총 개수
+    public int killedEnemies = 0; // 죽인 적 개수
 
-    public int totalEnemies = 0; //해당 스테이지 적 총 개수
-    public int killedEnemies = 0; //죽인 적 개수
-    //액션으로 몬스터 죽을때마다 값을 받아와서 차감한다.
-    //0이 되면 스테이지매니저에 정보 전달한다.
-    
     public Action OnEnemyCountDown;
     public Action OnEnemyCountZero;
 
+    [SerializeField] private float spawnDistance = 1.5f;
+
     private void Start()
     {
-        //스테이지매니저의 챕터SO를 참조한다.
         RegenStagesEnemy();
         GameEventsManager.Instance.enemyEvents.onEnemyKilled += EnemyKilled;
     }
 
     public void RegenStagesEnemy()
     {
-        killedEnemies = 0; //초기화
+        killedEnemies = 0; // 초기화
         curChapterSO = StageManager.Instance.chapterSOs[StageManager.Instance.curChapterIndex];
         enemySOs = curChapterSO.stageSOs[StageManager.Instance.curStageIndexInThisChapter].enemySOs;
         totalEnemies = curChapterSO.stageSOs[StageManager.Instance.curStageIndexInThisChapter].enemySOs.Length;
 
         for (int i = 0; i < enemySOs.Length; i++)
         {
-            Transform enemyRegenPos = enemyRegenPoss[i];
             EnemySO enemySO = enemySOs[i];
-            RegenEnemy(enemySO, enemyRegenPos);
+            Vector3 spawnPosition = transform.position + new Vector3(i * spawnDistance, 0, 0);
+            RegenEnemy(enemySO, spawnPosition);
         }
     }
 
-    public void RegenEnemy(EnemySO enemySO, Transform enemyRegenPos)
+    public void RegenEnemy(EnemySO enemySO, Vector3 spawnPosition)
     {
         GameObject obj = ObjectPool.Instance.GetObject("Enemy");
         Enemy enemy = SetUnitObject(obj);
         enemy.enemySO = enemySO;
         enemy.SetupEnemy();
-        enemy.transform.position = enemyRegenPos.position;
-        enemy.transform.SetParent(field, true);
+        enemy.transform.position = spawnPosition;
+
+        // 적 이동을 관리하는 컴포넌트 추가
+        EnemyMover enemyMover = enemy.GetComponent<EnemyMover>();
+        if (enemyMover == null)
+        {
+            enemyMover = enemy.gameObject.AddComponent<EnemyMover>();
+        }
+        enemyMover.SetMoveSpeed(2.0f); // 왼쪽으로 이동 속도 설정
     }
 
-    //정보 다 처리하고 셋엑티브오프하는 매서드 필요.
     private Enemy SetUnitObject(GameObject obj)
     {
-        Enemy enemy = obj.GetComponent<Enemy>();
-        return enemy;
+        return obj.GetComponent<Enemy>();
     }
 
     public void EnemyKilled()
@@ -65,9 +66,7 @@ public class RegenManager : Singleton<RegenManager>
 
         if (killedEnemies >= totalEnemies)
         {
-            //RegenStagesEnemy();
             StageManager.Instance.StageClear();
         }
     }
-
 }
