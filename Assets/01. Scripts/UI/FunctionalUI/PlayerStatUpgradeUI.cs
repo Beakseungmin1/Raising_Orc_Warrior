132c3b97ab2e0abdab685eb8f808b3f04181c937
Beakseungmin1 <dhkdwk1456@naver.com>
2025-01-06 12:39:43 +0900

    [feat] 스탯 업그레이드 꾹눌러서 업그레이드 기능 추가

--- Assets/01. Scripts/UI/FunctionalUI/PlayerStatUpgradeUI.cs
+++ Assets/01. Scripts/UI/FunctionalUI/PlayerStatUpgradeUI.cs
@@ -12,6 +12,19 @@ public class PlayerStatUpgradeUI : MonoBehaviour
     public UpgradeTabUI blueCriticalDamageUi;
     public UpgradeTabUI blueCriticalProbabilityUi;
 
+    private bool isAttackButtonDown;
+    private bool isHealthButtonDown;
+    private bool isHealtRegenButtonDown;
+    private bool isCriticalDamageButtonDown;
+    private bool isCriticalProbabilityButtonDown;
+    private bool isBlueCriticalDamageButtonDown;
+    private bool isBlueCriticalProbabilityButtonDown;
+
+    private float holdDuration = 0.5f; // n초 동안 누르고 있으면 시작
+    private float repeatInterval = 0.05f; // n초 마다 실행
+    private float holdTimer = 0f;
+    private float repeatTimer = 0f;
+
     private PlayerStat playerstat;
 
     private void Start()
@@ -24,39 +37,228 @@ public class PlayerStatUpgradeUI : MonoBehaviour
         blueCriticalDamageUi.UpdateblueCriticalIncreaseDamageStatUI();
         blueCriticalProbabilityUi.UpdateblueCriticalProbabilityStatUI();
     }
+
+    private void Update()
+    {
+        if (isAttackButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnAttackLevelUpButton();
+                }
+            }
+        }
+        if (isHealthButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnHealthLevelUpButton();
+                }
+            }
+        }
+        if (isHealtRegenButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnHealthRegenLevelUpButton();
+                }
+            }
+        }
+        if (isCriticalDamageButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnCriticalDamageLevelUpButton();
+                }
+            }
+        }
+        if (isCriticalProbabilityButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnCriticalProbabilityLevelUpButton();
+                }
+            }
+        }
+        if (isBlueCriticalDamageButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnBlueCriticalDamageLevelUpButton();
+                }
+            }
+        }
+        if (isBlueCriticalProbabilityButtonDown)
+        {
+            holdTimer += Time.deltaTime;
+            repeatTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                if (repeatTimer >= repeatInterval)
+                {
+                    repeatTimer = 0f;
+                    OnBlueCriticalProbabilityLevelUpButton();
+                }
+            }
+        }
+
+    }
+
     public void OnAttackLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.AttackLevelUp();
         attackUi.UpdateAttackStatUI();
     }
+
+    public void AttackPointerDown()
+    {
+        isAttackButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void AttackPointerUp()
+    {
+        isAttackButtonDown = false;
+        holdTimer = 0f;
+    }
+
     public void OnHealthLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.HealthLevelUp();
         healthUi.UpdateHealthStatUI();
     }
+
+    public void HealthPointerDown()
+    {
+        isHealthButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void HealthPointerUp()
+    {
+        isHealthButtonDown = false;
+        holdTimer = 0f;
+    }
+
+
     public void OnHealthRegenLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.HealthRegenerationLevelUp();
         healthRegenUi.UpdateHealthRegenerationUI();
     }
+
+    public void HealthRegenPointerDown()
+    {
+        isHealtRegenButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void HealthRegenPointerUp()
+    {
+        isHealtRegenButtonDown = false;
+        holdTimer = 0f;
+    }
+
     public void OnCriticalDamageLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.CriticalIncreaseDamageLevelUp();
         criticalDamageUi.UpdateCriticalIncreaseDamageUI();
     }
+
+    public void CriDamagePointerDown()
+    {
+        isCriticalDamageButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void CriDamagePointerUp()
+    {
+        isCriticalDamageButtonDown = false;
+        holdTimer = 0f;
+    }
+
     public void OnCriticalProbabilityLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.CriticalProbabilityLevelUp();
         criticalProbabilityUi.UpdateCriticalProbabilityUI();
     }
+
+    public void CriProbabilityPointerDown()
+    {
+        isCriticalProbabilityButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void CriProbabilityPointerUp()
+    {
+        isCriticalProbabilityButtonDown = false;
+        holdTimer = 0f;
+    }
+
+
     public void OnBlueCriticalDamageLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.BlueCriticalIncreaseDamageLevelUp();
         blueCriticalDamageUi.UpdateblueCriticalIncreaseDamageStatUI();
     }
+
+    public void BlueCriDamagePointerDown()
+    {
+        isBlueCriticalDamageButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void BlueCriDamagePointerUp()
+    {
+        isBlueCriticalDamageButtonDown = false;
+        holdTimer = 0f;
+    }
+
     public void OnBlueCriticalProbabilityLevelUpButton()
     {
         PlayerObjManager.Instance.Player.stat.BlueCriticalProbabilityLevelUp();
         blueCriticalProbabilityUi.UpdateblueCriticalProbabilityStatUI();
+    }
+
+    public void BlueCriProbabilityPointerDown()
+    {
+        isBlueCriticalProbabilityButtonDown = true;
+        holdTimer = 0f;
+    }
+
+    public void BlueCriProbabilityPointerUp()
+    {
+        isBlueCriticalDamageButtonDown = false;
+        holdTimer = 0f;
     }
 }
