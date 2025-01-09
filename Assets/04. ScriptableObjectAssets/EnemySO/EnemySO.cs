5a3041b7f0abacc7e0da3e3a31380c53a82cea95
leehokyun <urea42@naver.com>
2025-01-09 16:06:39 +0900

    [fix] 모바일 빌드시 적 HP 안뜨는 오류 수정했음

--- Assets/04. ScriptableObjectAssets/EnemySO/EnemySO.cs
+++ Assets/04. ScriptableObjectAssets/EnemySO/EnemySO.cs
@@ -14,11 +14,6 @@ public class EnemySO : ScriptableObject
     public GameObject model; //적 모델
     public float bossTimeLimit = 10f;
 
-    public BigInteger hp;
-    public BigInteger maxHp;
-    public BigInteger giveExp;
-    public BigInteger giveMoney;
-
     [Header("Skill Properties")]
     public float cooldown; // 쿨다운 시간 (보스가 스킬을 지니고 있을시 사용)
 
@@ -26,12 +21,4 @@ public class EnemySO : ScriptableObject
     public GameObject skillEffectPrefab; // 스킬 효과 프리팹
     public Collider2D effectRange; // 스킬 효과 범위
     public float damagePercent; // 액티브 스킬: 범위 내 적에게 주는 공격력 비율 (%)
-
-    private void OnValidate() //인스펙터창에 올라왔을 때 호출 되는 코드
-    {
-        hp = BigInteger.Parse(hpString);
-        maxHp = BigInteger.Parse(maxHpString);
-        giveExp = BigInteger.Parse(giveExpString);
-        giveMoney = BigInteger.Parse(giveMoneyString);
-    }
 }
