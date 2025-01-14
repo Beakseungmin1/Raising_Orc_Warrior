f182597a37d52f5ea773da8ffdd3611d8c3f88a7
kkamsogod <9310rlawlgns@gmail.com>
2025-01-14 20:22:33 +0900

    [Feat] 악세사리, 무기 데이터 SO에 색상 변경 추가

--- Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
+++ Assets/04. ScriptableObjectAssets/AccessorySO/AccessoryDataSO.cs
@@ -16,4 +16,19 @@ public class AccessoryDataSO : BaseItemDataSO
 
     [Header("Fuse Info")]
     public int requireFuseItemCount = 5;
+
+    public Color GetGradeColor(Grade grade)
+    {
+        switch (grade)
+        {
+            case Grade.Normal: return Color.white;
+            case Grade.Uncommon: return new Color(0.22f, 0.92f, 0.54f);
+            case Grade.Rare: return new Color(1f, 0.62f, 0.28f);
+            case Grade.Hero: return new Color(0.24f, 0.58f, 1f);
+            case Grade.Legendary: return new Color(0.75f, 0.25f, 1f);
+            case Grade.Mythic: return new Color(1f, 0.2f, 0.2f);
+            case Grade.Ultimate: return new Color(1f, 1f, 0.24f);
+            default: return Color.white;
+        }
+    }
 }
