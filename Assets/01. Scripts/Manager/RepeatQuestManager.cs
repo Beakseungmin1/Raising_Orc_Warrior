ac3acf01a6a660fb52ebcd9c0ee1a4336599612c
leehokyun <urea42@naver.com>
2025-01-14 16:36:45 +0900

    [add] RepeatQuestManager추가

---
+++ Assets/01. Scripts/Manager/RepeatQuestManager.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Numerics; // BigInteger »ç¿ë
+using UnityEngine;
+
+public class RepeatQuestManager : Singleton<RepeatQuestManager>
+{
+    //private const string LastQuestResetKey = "LastQuestResetDate";
+    //private DateTime nextResetTime;
+
+    [SerializeField] private QuestInfoSO[] questInfos;
+
+    private string[] questIds;
+    private QuestState[] currentQuestStates;
+
+    private void Awake()
+    {
+        questIds = new string[questInfos.Length];
+        currentQuestStates = new QuestState[questIds.Length];
+
+        for (int i = 0; i < questInfos.Length; i++)
+        {
+            questIds[i] = questInfos[i].id;
+        }
+    }
+
+    private void OnEnable()
+    {
+        GameEventsManager.Instance.questEvents.onQuestStateChange += QuestStateChange;
+    }
+    private void OnDisable()
+    {
+        GameEventsManager.Instance.questEvents.onQuestStateChange -= QuestStateChange;
+    }
+
+    public void StartQuest()
+    {
+        for (int i = 0; i < questInfos.Length; i++)
+        {
+            if (currentQuestStates[i].Equals(QuestState.CAN_START))
+            {
+                GameEventsManager.Instance.questEvents.StartQuest(questIds[i]);
+            }
+        }
+    }
+
+    public void FinishQuest(string id)
+    {
+        for (int i = 0; i < questInfos.Length; i++)
+        {
+            if (questIds[i] == id && currentQuestStates[i].Equals(QuestState.CAN_FINISH))
+            {
+                GameEventsManager.Instance.questEvents.FinishQuest(questIds[i]);
+                SoundManager.Instance.PlaySFX(SFXType.QuestReward);
+
+                GameEventsManager.Instance.questEvents.ReStartQuest(questIds[i]);
+            }
+        }
+    }
+
+    private void QuestStateChange(Quest quest)
+    {
+        for (int i = 0; i < questIds.Length; i++)
+        {
+            if (quest.info.id.Equals(questIds[i]))
+            {
+                currentQuestStates[i] = quest.state;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < questInfos.Length; i++)
+        {
+            currentQuestStates[i] = QuestState.CAN_START;
+        }
+        StartQuest();
+        //CalculateNextResetTime();
+        //CheckAndResetQuests();
+    }
+
+    /*
+    private void Update()
+    {
+        if (DateTime.Now >= nextResetTime)
+        {
+            ResetDailyQuests();
+            CalculateNextResetTime();
+        }
+    }
+
+    private void CalculateNextResetTime()
+    {
+        DateTime now = DateTime.Now;
+        nextResetTime = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0).AddDays(1);
+        Debug.Log($"Next reset time calculated: {nextResetTime}");
+    }
+
+    private void CheckAndResetQuests()
+    {
+        if (!PlayerPrefs.HasKey(LastQuestResetKey))
+        {
+            // First-time setup: Reset quests immediately
+            ResetDailyQuests();
+            return;
+        }
+
+        string lastResetDateStr = PlayerPrefs.GetString(LastQuestResetKey);
+        DateTime lastResetDate = DateTime.Parse(lastResetDateStr);
+
+        if (lastResetDate.Date < DateTime.Now.Date)
+        {
+            ResetDailyQuests();
+        }
+    }
+
+
+    private void ResetDailyQuests()
+    {
+        for (int i = 0; i < questInfos.Length; i++)
+        {
+            currentQuestStates[i] = QuestState.CAN_START;
+        }
+        StartDailyQuest();
+
+        PlayerPrefs.SetString(LastQuestResetKey, DateTime.Now.Date.ToString());
+        PlayerPrefs.Save();
+    }
+    */
+}
