c09e705800399785c2639f2fcea3f69b014828c8
leehokyun <urea42@naver.com>
2025-01-15 22:14:04 +0900

    [fix] EquipmentUI 탭 버튼 누르면 UI 컬러로 opened, closed 구분

--- Assets/01. Scripts/UI/Main_EquipmentUI.cs
+++ Assets/01. Scripts/UI/Main_EquipmentUI.cs
@@ -1,12 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main_EquipmentUI : UIBase
 {
+    public Sprite openedTabSprite;
+    public Sprite closedTabSprite;
+
+    public Image weaponTab;
+    public Image accTab;
+
+    public TextMeshProUGUI weaponLabel;
+    public TextMeshProUGUI accLabel;
+
+    bool isWeaponTab = true;
+
+    private void OnEnable()
+    {
+        isWeaponTab = true;
+        weaponTab.sprite = openedTabSprite;
+        accTab.sprite = closedTabSprite;
+        weaponLabel.color = Color.black;
+        accLabel.color = Color.white;
+    }
+
     public void ShowEquipmentUpgradePopupUI()
     {
         UIManager.Instance.Show<DimmedUI>();
         UIManager.Instance.Show<EquipmentUpgradePopupUI>();
+    }
+
+    public void OnClickWeaponTab()
+    {
+        if (!isWeaponTab)
+        {
+            weaponTab.sprite = openedTabSprite;
+            accTab.sprite = closedTabSprite;
+            weaponLabel.color = Color.black;
+            accLabel.color = Color.white;
+            isWeaponTab = true;
+        }
+    }
+
+    public void OnClickAccTab()
+    {
+        if (isWeaponTab)
+        {
+            weaponTab.sprite = closedTabSprite;
+            accTab.sprite = openedTabSprite;
+            weaponLabel.color = Color.white;
+            accLabel.color = Color.black;
+            isWeaponTab = false;
+        }
     }
 }
