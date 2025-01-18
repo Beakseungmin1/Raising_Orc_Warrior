9d38f6ea3399e1df8a9a814d863650549a808ea9
leehokyun <urea42@naver.com>
2025-01-19 00:47:52 +0900

    [refactor] DungeonUI_confirmEnterBtnPopUpUi 상속구조로 변경

---
+++ Assets/01. Scripts/UI/DungeonUI_ConfirmEnterBtnPopUpUI.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+
+public class DungeonUI_ConfirmEnterBtnPopUpUI : UIBase
+{
+    public DungeonInfoSO dungeonInfoSO;
+
+    public TextMeshProUGUI curLevel;
+    public TextMeshProUGUI dungeonTicketCount;
+
+    public void Init()
+    {
+        curLevel.text = $"{dungeonInfoSO.level}구역";
+        dungeonTicketCount.text = $"{CurrencyManager.Instance.GetCurrency(CurrencyType.DungeonTicket)}/1";
+    }
+
+    public void ExitBtn()
+    {
+        Hide();
+        UIManager.Instance.Hide<DimmedUI>();
+    }
+
+    public void OnGoToDungeonBtnClick()
+    {
+        if (CurrencyManager.Instance.GetCurrency(CurrencyType.DungeonTicket) >= 1)
+        {
+            Hide();
+
+            string uiName = dungeonInfoSO.type.ToString() + "UI";
+            UIManager.Instance.Hide(uiName);
+            UIManager.Instance.Hide<StageInfoUI>();
+            UIManager.Instance.Hide<DimmedUI>();
+            UIManager.Instance.Show<Main_PlayerUpgradeUI>();
+            UIManager.Instance.Show<BossStageInfoUI>();
+ 
+            GameEventsManager.Instance.dungeonEvents.DungeonUIChanged();
+
+            CurrencyManager.Instance.SubtractCurrency(CurrencyType.DungeonTicket, 1);
+            StageManager.Instance.GoToDungeonStage(dungeonInfoSO.type, dungeonInfoSO.level);
+        }
+    }
+}
--- Assets/01. Scripts/UI/GoldDungeonUI_ConfirmEnterBtnPopUpUI.cs
+++ Assets/01. Scripts/UI/GoldDungeonUI_ConfirmEnterBtnPopUpUI.cs
@@ -1,37 +1,7 @@
 using UnityEngine;
 using TMPro;
 
-public class GoldDungeonUI_ConfirmEnterBtnPopUpUI : UIBase
+public class GoldDungeonUI_ConfirmEnterBtnPopUpUI : DungeonUI_ConfirmEnterBtnPopUpUI
 {
-    public DungeonInfoSO dungeonInfoSO;
 
-    public TextMeshProUGUI curLevel;
-    public TextMeshProUGUI dungeonTicketCount;
-
-    public void Init()
-    {
-        curLevel.text = $"{dungeonInfoSO.level}±¸¿ª";
-        dungeonTicketCount.text = $"{CurrencyManager.Instance.GetCurrency(CurrencyType.DungeonTicket)}/1";
-    }
-
-    public void ExitBtn()
-    {
-        Hide();
-        UIManager.Instance.Hide<DimmedUI>();
-    }
-
-    public void OnGoToDungeonBtnClick()
-    {
-        if (CurrencyManager.Instance.GetCurrency(CurrencyType.DungeonTicket) >= 1)
-        {
-            Hide();
-            UIManager.Instance.Hide<GoldDungeonUI>();
-            UIManager.Instance.Hide<StageInfoUI>();
-            UIManager.Instance.Hide<DimmedUI>();
-            UIManager.Instance.Show<Main_PlayerUpgradeUI>();
-            UIManager.Instance.Show<BossStageInfoUI>();
-            CurrencyManager.Instance.SubtractCurrency(CurrencyType.DungeonTicket, 1);
-            StageManager.Instance.GoToDungeonStage(dungeonInfoSO.type, dungeonInfoSO.level);
-        }
-    }
 }
